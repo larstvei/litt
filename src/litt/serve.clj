b165eaa6bbf50ff1455def75b01d4b48ce7af4c5
larstvei <larstvei@ifi.uio.no>
2025-02-09 17:32:29 +0100

    CSS being embedded, there is no point in serving it

--- src/litt/serve.clj
+++ src/litt/serve.clj
@@ -10,23 +10,13 @@
   ((zipmap (map (comp first fs/split-ext) (keys lit)) (keys lit))
    uri))
 
-(defn resolve-css [{:sources/keys [css] :as db} uri]
-  ((set (keys css)) uri))
-
 (defn app [db {:keys [uri] :as req}]
-  (let [uri (s/replace-first uri "/" "")
-        md (resolve-lit db uri)
-        css (resolve-css db uri)]
-    (cond
-      md
+  (let [uri (s/replace-first uri "/" "")]
+    (when-let [md (resolve-lit db uri)]
       {:status  200
        :headers {"Content-Type" "text/html"}
        :body    (->> (get-in db [:sources/lit md :file/content])
-                     (typesetting/md-file->html db))}
-      css
-      {:status  200
-       :headers {"Content-Type" "text/css"}
-       :body    (get-in db [:sources/css css :file/content])})))
+                     (typesetting/md-file->html db))})))
 
 (defonce server
   (server/run-server (fn [req] (app @db/db req)) {:port 8080}))
